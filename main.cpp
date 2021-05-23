91379af56317d044aee89a80fce09c0f6a2b3bb2
MasloNS <PRIb-201_879358@volsu.ru>
2021-05-23 18:19:18 +0300

    commi

--- main.cpp
+++ main.cpp
@@ -7,9 +7,8 @@ int main()
 	cout << "Enter A and B: ";
 	cin >> a >> b;
 	cout << "A + B = " << a + b << '\n'
-	 << "A - B = " << a - b << '\n'
-	 << "A / B = " << a - b << '\n'
-	 << "A * B = " << a * b << '\n';
-
+	     << "A - B = " << a - b << '\n'
+	     << "A / B = " << a - b << '\n'
+	     << "A * B = " << a * b << '\n';
     return 0;
 }
