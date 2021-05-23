a326e32417cc83c4d9366209c20955ec421dae64
MasloNS <PRIb-201_879358@volsu.ru>
2021-05-23 17:12:41 +0300

    div

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@ int main()
 	cin >> a >> b;
 	cout << "A + B = " << a + b << '\n';
 	cout << "A - B = " << a - b << '\n';
-	cout << "A - B = " << a - b << '\n';
+	cout << "A - B = " << a - b << '\n'
 	cout << "A * B = " << a * b << '\n';
+
     return 0;
 }
