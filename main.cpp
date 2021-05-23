48267d4c4d1928b20f4700a1221cd75e07d9172f
MasloNS <PRIb-201_879358@volsu.ru>
2021-05-23 16:21:40 +0300

    добавлены операции

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
 	cin >> a >> b;
 	cout << "A + B = " << a + b << '\n';
 	cout << "A - B = " << a - b << '\n';
-	cout << "A * B = " << a * b << '\n';
+	cout << "A * B = " << a * b << '\n'
 	cout << "A / B = " << a - b << '\n';
 
     return 0;
